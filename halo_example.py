7caad2470371f49e08f15a7a0ee55dc024556042
Dean Hall <deanhall.aero@gmail.com>
2025-11-09 00:25:54 -0500

    Update halo_example.py
    Wrong file

--- halo_example.py
+++ halo_example.py
@@ -1,15 +1,15 @@
 import numpy as np
-from quasi_orbit_approx import third_order_solution_lissajous, plot_orbit_approx
+from quasi_orbit_approx import fourth_order_solution_halo, plot_orbit_approx
 import matplotlib.pyplot as plt
 
-Az = 5000
-Ay = 5000
-m = 1e-5
-tf = 20 * np.pi
+Az = 34000
+m = 1e-11
+tf = 40 * np.pi
 num_points = 10000
-theta1 = 0
-theta2 = 0
+theta = 0
+
 date = [10, 19, 2025, 0, 0, 0]  # Month, Day, Year, Hour, Minute, Second
 
-x, y, z = third_order_solution_lissajous(Ay, Az, m, tf, num_points, theta1, theta2, date)
+x, y, z = fourth_order_solution_halo(Az, m, tf, num_points, theta, date)
 plot_orbit_approx(x,y,z, "halo")
+
