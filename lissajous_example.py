359031e7a445daeadf5ad23f4ea97e81cd338bed
Dean Hall <deanhall.aero@gmail.com>
2025-11-09 00:26:14 -0500

    Update lissajous_example.py
    Added plot function

--- lissajous_example.py
+++ lissajous_example.py
@@ -1,5 +1,5 @@
 import numpy as np
-from quasi_orbit_approx import third_order_solution_lissajous
+from quasi_orbit_approx import third_order_solution_lissajous, plot_orbit_approx
 import matplotlib.pyplot as plt
 
 Az = 5000
@@ -12,32 +12,4 @@ theta2 = 0
 date = [10, 19, 2025, 0, 0, 0]  # Month, Day, Year, Hour, Minute, Second
 
 x, y, z = third_order_solution_lissajous(Ay, Az, m, tf, num_points, theta1, theta2, date)
-
-plt.figure(figsize=(8,8))
-plt.plot(x, y)
-plt.axis('equal')
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.grid()
-
-plt.figure(figsize=(8,8))
-plt.plot(x, z)
-plt.axis('equal')
-plt.xlabel("X")
-plt.ylabel("Z")
-plt.grid()
-
-plt.figure(figsize=(8,8))
-plt.plot(y, z)
-plt.axis('equal')
-plt.xlabel("Y")
-plt.ylabel("Z")
-plt.grid()
-
-fig, ax = plt.subplots(subplot_kw={"projection":"3d"})
-ax.plot(x,y,z, "b")
-ax.scatter(0,0, 0, color = "black", marker="*", label="L2 Point")
-ax.set_xlabel("X")
-ax.set_ylabel("Y")
-ax.set_zlabel("Z")
-plt.show()
+plot_orbit_approx(x,y,z, "halo")
